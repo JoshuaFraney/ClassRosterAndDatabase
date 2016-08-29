a7ff4423db581ea6235038df675c2f45b6db93bf
Joshua Franey <joshua.franey@gmail.com>
2016-08-29 16:34:08 -0400

    Class Roster And Database Assignment

---
+++ src/ssa/Mainline.java
@@ -0,0 +1,57 @@
+package ssa;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Mainline {
+
+	public static void main(String[] args) {
+		ClassRoster stank = new ClassRoster();
+		stank.classRosterReport();
+	}
+
+//	private static void classRosterReport() {
+//		ArrayList<String> classList = new ArrayList<String>();
+//		classList.add("Joshua");
+//		classList.add("Michael S");
+//		classList.add("Jarrett");
+//		classList.add("Karen");
+//		classList.add("Shaquil");
+//		classList.add("Aisha");
+//		classList.add("Evan");
+//		classList.add("Daniel");
+//		classList.add("Kevin");
+//		classList.add("Joseph");
+//		classList.add("Li");
+//		classList.add("Steven");
+//		classList.add("Stephen");
+//		classList.add("Peter");
+//		classList.add("Michael C.");
+//		classList.add("Reuben");
+//		classList.add("Dax");
+//		classList.add("Arun");
+//		classList.add("Gabriel");
+//		classList.add("Jonathan");
+//		classList.add("Kyle");
+//
+//		Collections.sort(classList);
+//		
+//		for(String cl : classList) {
+//			System.out.println(cl);
+//		}
+//		System.out.println("*******************************************");
+//		int classCount = classList.size();
+//		System.out.println("Total number of students is " + classCount + ".");
+//		System.out.println("*******************************************");
+//
+//		Collections.sort(classList, Collections.reverseOrder());
+//		
+//		for(String lc : classList)
+//		System.out.println(lc);
+//		System.out.println("*******************************************");
+//	}
+//	
+//			
+	}
+	
